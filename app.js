13d65f12c201d64ab3d929dc6e11b59e27f4cacf
Lautaro Cepeda <lautac29@gmail.com>
2021-05-04 17:37:20 -0300

    Added logger and nodemon

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const CONFIG    = require('./config/config');
+const logger    = require('morgan')
 const express   = require('express');
 const app       = express();
 const port      = CONFIG.port;
 
 
-app.get('/', (req, res) => {
-    res.send('Server On');
+// http logger morgan
+app.use(logger('dev'));
+
+
+app.use('/', (req, res) => {
+    res.statusCode = 200;
+    res.json( { status: 'success', message: 'Pending API', data: {} } );
 });
+
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
